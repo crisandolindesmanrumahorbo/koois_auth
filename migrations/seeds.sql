e4b0c6d7dfc5d8bdb169d1d4369c29979ba5081c
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-10-20 13:19:24 +0700

    add see menus permissions

--- migrations/seeds.sql
+++ migrations/seeds.sql
@@ -21,3 +21,20 @@ JOIN permissions p ON (
     (r.name = 'learner' AND p.name IN ('join_room', 'complete_quiz'))
 );
 
+
+INSERT INTO permissions (name, description)
+VALUES
+  ('menu_user', 'Role can access menu user'),
+  ('menu_create_quiz', 'Role can access menu create quiz'),
+  ('menu_quiz', 'Role can access menu quiz');
+
+
+INSERT INTO role_permissions (role_id, permission_id)
+SELECT r.role_id, p.permission_id
+FROM roles r
+JOIN permissions p ON (
+    (r.name = 'admin' AND p.name IN ('menu_user')) OR
+    (r.name = 'facilitator' AND p.name IN ('menu_create_quiz')) OR
+    (r.name = 'learner' AND p.name IN ('menu_quiz'))
+);
+
